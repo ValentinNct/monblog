92774026bf1e0107d25b5e1167d10eeec247b197
nicot <valentin.nicot29@gmail.com>
2023-10-04 15:55:02 +0200

    Modification Modele

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-	// Accès aux données
-    $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','root', '');
-    $billets = $bdd->query('select idBillet as id, dateBillet as date,'
-                        . ' titreBillet as titre, contenuBillet as contenu from BILLET'
-                        . ' order by idBillet desc');
+	require 'Modele.php';
+	$billets=getBillets();
 // Affichage
 require 'vueAcceuil.php';

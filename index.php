459276e0c26ef7acd83eb32b1493dc680c621680
nicot <valentin.nicot29@gmail.com>
2023-10-16 16:24:38 +0200

    Sécurité2

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<?php
-<<<<<<< HEAD
-	require 'Controleur.php';
-	try {
-		if (isset($_GET['action'])) {
-			//echo $_GET
-			if($_GET['action'] == 'billet'){
-			$idBillet = $_GET['id'];
-			unBillet($idBillet);
-			}
-			else {
-				// une autre action demandée
-				erreur("Action non validée");
-			}
-		}
-		else {
-		// pas d'action demandée : affichage par défaut
-		accueil();
-		}
-	} catch (Exception $e) {
-		$msgErreur = $e->getMessage();
-	}
-=======
-    require 'Controleur/Controleur.php';
+  <?php 
+   require 'Controleur/Controleur.php';
 
     try{
         if(isset($_GET['action'])){
@@ -51,4 +29,3 @@
     }
 ?>
 
->>>>>>> main
